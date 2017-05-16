d258de9969c0111f4c20560b92ac96440fd124f6
Ana Mikaela A. Vilar <anamikavilar@gmail.com>
2017-05-16 10:44:02 +0800

    Update mika.js

--- javascript/mika.js
+++ javascript/mika.js
@@ -1,3 +1,46 @@
-function mika(){
- document.getElementById("mm").innerHTML= " <p> hello </p>";
+function account(){
+	document.getElementById("content").innerHTML= '<div class = "bgcontent" ><div class="contentcontent">\n\
+					<h2 class = "welcome">Welcome GM</h2>\n\
+                   <div class="figure">\n\
+                        </div>\n\
+                    </div>\n\
+                </div></div>';
+}
+
+function guard_list(){
+	document.getElementById("content").innerHTML= "<h2>Guard Master List</h2>\n\
+		<table class='fixed' border ='1'> \n\
+			<tr> \n\
+				<th>Guard ID</th>\n\
+				<th>Full Name</th>\n\
+				<th>Residing Address</th>\n\
+				<th>Regular Post</th>\n\
+				<th>Leave Status</th>	\n\
+			</tr>\n\
+			<tr>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+			</tr>\n\
+		</table>";
+}
+
+function post_list(){
+	document.getElementById("content").innerHTML= "<h2>Post Master List</h2>\n\
+		<table class='fixed' border ='1'> \n\
+			<tr> \n\
+				<th>Post ID</th>\n\
+				<th>Post Name</th>\n\
+				<th>Post Address</th>\n\
+				<th>Contact Number</th>\n\
+			</tr>\n\
+			<tr>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+				<td>f</td>\n\
+			</tr>\n\
+		</table>";
 }
